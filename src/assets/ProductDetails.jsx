50504718cc40021f095bdaf6a0684d1ed3dafec5
Dux <kartikmadaan1907@gmail.com>
2023-04-30 15:03:53 +0530

    Added responsiveness

--- src/assets/ProductDetails.jsx
+++ src/assets/ProductDetails.jsx
@@ -7,6 +7,7 @@ const ProductDetails = () => {
   const products = useSelector((state) => state.allSelectedProducts);
   const { productId } = useParams();
   const { title, price, description, category, image } = products;
+  
 
   const dispatch = useDispatch();
   useEffect(() => {
@@ -35,10 +36,10 @@ const ProductDetails = () => {
         >
           <div className="col-10 fs-4">
             <div className="row justify-content-center align-items-center">
-              <div className="col-5">
+              <div className="col-md-5 col-8 m-5">
                 <img className="img-fluid" src={image} alt={title} />
               </div>
-              <div className="col-5">
+              <div className="col-md-5 col-8">
                 <div className="title m-2">
                   <span className="fw-bold">Product:</span> {title}
                 </div>
@@ -48,7 +49,7 @@ const ProductDetails = () => {
                 <div className="category m-2">
                   <span className="fw-bold">Category: </span> {category}
                 </div>
-
+               
                 <div className="price m-2">
                   <span className="fw-bold">Price: </span> ${price}
                 </div>
